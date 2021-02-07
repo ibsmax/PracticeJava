a66bfaff50724d05e32ffe02c4fda0b1f57b7d93
Max <ibsmax23@gmail.com>
2021-02-07 15:46:23 +0500

    Changes Task 3

--- src/com/company/Task3.java
+++ src/com/company/Task3.java
@@ -2,17 +2,19 @@ package com.company;
 
 public class Task3 {
     public static void main(String[] args) {
-        //Пункт 1
+        //Пункт 1 (Задайте массив из 5 любых целых чисел)
         int[] massive = {1, 2, 3, 4, 5};
 
-        //Пункт 2
-        int first = massive[4];
-        int last = massive[0];
-        int[] massive2 = {first,2,3,4,last};
+        //Пункт 2 (Поменяйте местами первый и последний элемент в массиве)
+        int tmp = massive[0];
+        massive[0] = massive[4];
+        massive[4] =tmp;
+        System.out.println(massive[0]);
+        System.out.println(massive[4]);
 
-        //Пункт 3
-        int a =massive2 [massive2.length -1];
-        int b =massive2 [massive2.length -3];
+        //Пункт 3 (Вывести в консоль результат суммы первого и среднего элемента)
+        int a = massive[massive.length -5];
+        int b = massive[massive.length -3];
         int sum = a + b;
         System.out.println(sum);
         }
